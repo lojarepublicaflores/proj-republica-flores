4ada5c55cd12b3a780a5ea35f85e656029aa1222
edivaldo.santos <edivaldo.santos@engdb.com.br>
2016-08-19 14:34:57 -0300

    altera nome arquivo

---
+++ teste2/src/teste2/arquivo1.java
@@ -0,0 +1,22 @@
+package teste2;
+
+public class arquivo1 {
+
+	public arquivo1() {
+		// TODO Auto-generated constructor stub
+		///1 edite  edivaldo
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//edivaldo soares
+///2 edite edivaldo pelo crome
+		///edit eclipse
+
+
+
+	// TODO Auto-generated method stub
+	}
+
+}
